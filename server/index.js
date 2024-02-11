361e2ab61721f72b5f32f11f13a6d654ac9c94ba
unknown <harshitha61011@gmail.com>
2024-02-11 15:01:25 +0530

    complete memories

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use("/user", userRouter);
+
+const CONNECTION_URL = 'mongodb+srv://Harshitha:KlYSByG2cHlhghGS@cluster0.kr6mdax.mongodb.net/';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
